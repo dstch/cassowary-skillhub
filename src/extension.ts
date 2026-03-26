794ae1d97d31a4773c46d5f36f698f4e59495636
Developer <developer@skillshub.com>
2026-03-26 19:09:00 +0800

    feat: add Skill Explorer sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -3,16 +3,30 @@ import { SkillManager } from './services/SkillManager';
 import { MarketplaceService } from './services/MarketplaceService';
 import { Logger } from './services/logger';
 import { registerCommands } from './commands';
+import { SkillExplorerProvider, SkillTreeItem } from './ui/SkillExplorer';
+import { Skill } from './services/types';
 
 let skillManager: SkillManager;
-let marketplaceService: MarketplaceService;
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   Logger.info('Skillshub activated');
   skillManager = new SkillManager();
-  marketplaceService = new MarketplaceService();
+  const marketplaceService = new MarketplaceService();
   const commands = registerCommands(skillManager, marketplaceService);
   commands.forEach(cmd => context.subscriptions.push(cmd));
+
+  const skills = await skillManager.list();
+  const provider = new SkillExplorerProvider(skills);
+  vscode.window.registerTreeDataProvider('skillExplorer', provider);
+
+  vscode.commands.registerCommand('skillshub.refreshExplorer', async () => {
+    const updatedSkills = await skillManager.list();
+    provider.refresh(updatedSkills);
+  });
+
+  vscode.commands.registerCommand('skillshub.openSkill', async (skill: Skill) => {
+    vscode.window.showTextDocument(vscode.Uri.file(skill.path));
+  });
 }
 
 export function deactivate() {}
---
+++ src/ui/SkillExplorer.ts
@@ -0,0 +1,36 @@
+import * as vscode from 'vscode';
+import { Skill } from '../services/types';
+
+export class SkillExplorerProvider implements vscode.TreeDataProvider<SkillTreeItem> {
+  private _onDidChangeTreeData = new vscode.EventEmitter<SkillTreeItem | undefined>();
+  readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+
+  constructor(private skills: Skill[]) {}
+
+  refresh(skills: Skill[]) {
+    this.skills = skills;
+    this._onDidChangeTreeData.fire(undefined);
+  }
+
+  getTreeItem(element: SkillTreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  async getChildren(): Promise<SkillTreeItem[]> {
+    return this.skills.map(skill => new SkillTreeItem(skill));
+  }
+}
+
+export class SkillTreeItem extends vscode.TreeItem {
+  constructor(public readonly skill: Skill) {
+    super(skill.name, vscode.TreeItemCollapsibleState.None);
+    this.description = skill.version;
+    this.contextValue = 'skill';
+    this.iconPath = new vscode.ThemeIcon('symbol-property');
+    this.command = {
+      command: 'skillshub.openSkill',
+      title: 'Open',
+      arguments: [this.skill]
+    };
+  }
+}
